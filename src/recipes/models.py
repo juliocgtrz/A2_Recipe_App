53da44c48af5e2380b898fc9fbe7b10591139c33
Julio G <juliocgtrz@gmail.com>
2024-07-15 21:17:19 -0600

    Update models.py
    added pic attribute to model and made recipe name clickable

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
 class Recipe(models.Model):
@@ -10,7 +11,8 @@ class Recipe(models.Model):
     )
     cooking_time = models.IntegerField(help_text="Enter cooking time in minutes")
     difficulty = None
-
+    pic = models.ImageField(upload_to='recipes', default='no_picture.jpg')
+    
     # determine recipe difficulty
     def difficulty(self):
         ingredients = self.ingredients.split(", ")
@@ -27,3 +29,7 @@ class Recipe(models.Model):
     # string representation
     def __str__(self):
         return str(self.name)
+
+    # primary key of recipe object becomes clickable
+    def get_absolute_url(self):
+        return reverse("recipes:detail", kwargs={"pk": self.pk})
