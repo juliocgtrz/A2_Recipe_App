62454fa2c2731d577fdd7504fbd3b62865cd57fd
Julio G <juliocgtrz@gmail.com>
2024-07-16 23:09:39 -0600

    Update views.py
    added arguments to protect pages

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView   #to display lists and details
 from .models import Recipe   #to access Recipe model
+from django.contrib.auth.mixins import LoginRequiredMixin   #to protect class-based view
+# from django.contrib.auth.decorators import login_required   #to protect function-based views
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/home.html')
     
-class RecipeListView(ListView):             #class-based view
-    model = Recipe                          #specify model
-    template_name = 'recipes/list.html'     #specify template
+class RecipeListView(LoginRequiredMixin, ListView):             #class-based "protected" view
+    model = Recipe                                              #specify model
+    template_name = 'recipes/list.html'                         #specify template
 
-class RecipeDetailView(DetailView):         #class-based view
-    model = Recipe                          #specify model
-    template_name = 'recipes/detail.html'   #specify template
+class RecipeDetailView(LoginRequiredMixin, DetailView):         #class-based "protected" view
+    model = Recipe                                              #specify model
+    template_name = 'recipes/detail.html'                       #specify template
