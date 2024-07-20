b7c80fb26334660954029b7e3d668113399c1ef4
Julio G <juliocgtrz@gmail.com>
2024-07-19 21:53:45 -0600

    Update settings.py
    removed Secret Key and Debug

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*6)n$6vgbq!)vfg#)*iqcx=)+-o0ss23k6ia-z@aakj_((@xve'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
